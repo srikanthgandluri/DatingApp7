442232f45e97ded815d784c07f9bb0b508c805d9
srikanthgandluri <srikanth.gandluri@gmail.com>
2023-07-07 00:31:43 +0530

    End of section 11

--- API/Controllers/AccountsController.cs
+++ API/Controllers/AccountsController.cs
@@ -45,7 +45,9 @@ namespace API.Controllers
         [HttpPost("login")]
         public async Task<ActionResult<UserDTO>> Login(LoginDTO loginDTO)
         {
-            var user = await _context.Users.SingleOrDefaultAsync(x=>x.UserName ==loginDTO.Username);
+            var user = await _context.Users
+            .Include(p=>p.Photos)
+            .SingleOrDefaultAsync(x=>x.UserName ==loginDTO.Username);
             if(user==null)
             return Unauthorized();
             using var hmac = new HMACSHA512(user.PasswordSalt);
@@ -57,7 +59,8 @@ namespace API.Controllers
             }
                return new UserDTO{
                     Username = user.UserName,
-                    Token = _tokenService.CreateToken(user)
+                    Token = _tokenService.CreateToken(user),
+                    PhotoUrl = user.Photos.FirstOrDefault(x=>x.IsMain)?.Url
             };
         }
 
